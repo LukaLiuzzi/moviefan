944dd38dee19d12df94e21d2cb092d92f70c8f84
Luka Liuzzi <lukaliuzzidev@gmail.com>
2022-01-31 18:20:56 -0300

    fetching data, search data

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,103 @@
-// Menu de navegacion de filtros
-const guideButton = document.querySelector(".guide-button");
-const guide = document.querySelector(".guide");
-
-guideButton.addEventListener("click", (e) => {
-	guide.classList.toggle("left0");
-});
-
-window.addEventListener("scroll", (e) => {
-	guide.classList.remove("left0");
-});
-
-// Trayendo datos de la api
+// Declaration of variables
 const API_KEY = "api_key=e11854d9b2dd14d971cfa32f0cc594d7";
 const BASE_URL = "https://api.themoviedb.org/3/";
 const API_URL =
 	BASE_URL +
 	"discover/movie?" +
 	API_KEY +
-	"&language=es&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
+	"&primary_release_date.gte=2021-01-31&primary_release_date.lte=2022-01-31&language=es";
 
 const IMG_BASE_URL = "https://image.tmdb.org/t/p/w500";
+const SEARCH_URL = BASE_URL + "search/movie?" + API_KEY;
+const genres = {
+	genres: [
+		{
+			id: 28,
+			name: "Acción",
+		},
+		{
+			id: 12,
+			name: "Aventura",
+		},
+		{
+			id: 16,
+			name: "Animación",
+		},
+		{
+			id: 35,
+			name: "Comedia",
+		},
+		{
+			id: 80,
+			name: "Crimen",
+		},
+		{
+			id: 99,
+			name: "Documental",
+		},
+		{
+			id: 18,
+			name: "Drama",
+		},
+		{
+			id: 10751,
+			name: "Familia",
+		},
+		{
+			id: 14,
+			name: "Fantasía",
+		},
+		{
+			id: 36,
+			name: "Historia",
+		},
+		{
+			id: 27,
+			name: "Terror",
+		},
+		{
+			id: 10402,
+			name: "Música",
+		},
+		{
+			id: 9648,
+			name: "Misterio",
+		},
+		{
+			id: 10749,
+			name: "Romance",
+		},
+		{
+			id: 878,
+			name: "Ciencia ficción",
+		},
+		{
+			id: 10770,
+			name: "Película de TV",
+		},
+		{
+			id: 53,
+			name: "Suspense",
+		},
+		{
+			id: 10752,
+			name: "Bélica",
+		},
+		{
+			id: 37,
+			name: "Western",
+		},
+	],
+};
 
+const movies = document.getElementById("movies");
+const search = document.getElementById("search");
+const form = document.getElementById("form");
+const tags = document.getElementById("tags");
+
+// Functions
+
+// Fetching API
 const getMovies = (url) => {
 	fetch(url)
 		.then((res) => res.json())
@@ -30,19 +107,59 @@ const getMovies = (url) => {
 		});
 };
 
+// Showing data in DOM
 const showMovies = (data) => {
 	data.forEach((movie) => {
-		const { title, poster_path, vote_average, overview } = movie;
-		const card = `<div class="card">
-			<div class="movie-img">
-			<img src="${IMG_BASE_URL + poster_path}" alt="${title}" />
+		const { title, poster_path, vote_average, overview, release_date } = movie;
+		const card = `<div class="col-12 col-md-6 col-lg-4 col-xxl-2 mb-5">
+		<div class="card">
+			<img
+				src="${IMG_BASE_URL + poster_path}"
+				class="card-img-top"
+				alt="${title}"
+			/>
+			<div class="card-body">
+				<h5 class="card-title m-0 fs-1">${title}</h5>
+				<p class="card-text overview fs-3">
+				${overview}
+				</p>
 			</div>
-			<div class="movie-title">
-			<h3>${title}</h3>
-			</div> 
-			</div>`;
-		document.getElementById("movies").innerHTML += card;
+			<ul class="list-group list-group-flush">
+				<li class="list-group-item">Accion</li>
+				<li class="list-group-item ${getColor(vote_average)}">${vote_average}</li>
+				<li class="list-group-item">${release_date}</li>
+			</ul>
+		</div>
+	</div>`;
+		movies.innerHTML += card;
 	});
 };
 
+// Painting color according to result
+const getColor = (vote) => {
+	if (vote >= 7.5) {
+		return "text-success";
+	} else if (vote < 7.5 && vote >= 5) {
+		return "text-warning";
+	} else {
+		return "text-danger";
+	}
+};
+
+// Search and paint
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	searchTerm = search.value;
+
+	if (searchTerm) {
+		getMovies(SEARCH_URL + "&language=es&query=" + searchTerm);
+		movies.innerHTML = "";
+	} else {
+		movies.innerHTML = "";
+		getMovies(API_URL);
+	}
+});
+
+// Painting default results
 getMovies(API_URL);
