a11a7273dc5620b08f18c7adba0369079406089a
Luka Liuzzi <lukaliuzzidev@gmail.com>
2022-02-01 01:57:45 -0300

    genre filters, layout

--- public/js/main.js
+++ public/js/main.js
@@ -9,86 +9,84 @@ const API_URL =
 
 const IMG_BASE_URL = "https://image.tmdb.org/t/p/w500";
 const SEARCH_URL = BASE_URL + "search/movie?" + API_KEY;
-const genres = {
-	genres: [
-		{
-			id: 28,
-			name: "Acción",
-		},
-		{
-			id: 12,
-			name: "Aventura",
-		},
-		{
-			id: 16,
-			name: "Animación",
-		},
-		{
-			id: 35,
-			name: "Comedia",
-		},
-		{
-			id: 80,
-			name: "Crimen",
-		},
-		{
-			id: 99,
-			name: "Documental",
-		},
-		{
-			id: 18,
-			name: "Drama",
-		},
-		{
-			id: 10751,
-			name: "Familia",
-		},
-		{
-			id: 14,
-			name: "Fantasía",
-		},
-		{
-			id: 36,
-			name: "Historia",
-		},
-		{
-			id: 27,
-			name: "Terror",
-		},
-		{
-			id: 10402,
-			name: "Música",
-		},
-		{
-			id: 9648,
-			name: "Misterio",
-		},
-		{
-			id: 10749,
-			name: "Romance",
-		},
-		{
-			id: 878,
-			name: "Ciencia ficción",
-		},
-		{
-			id: 10770,
-			name: "Película de TV",
-		},
-		{
-			id: 53,
-			name: "Suspense",
-		},
-		{
-			id: 10752,
-			name: "Bélica",
-		},
-		{
-			id: 37,
-			name: "Western",
-		},
-	],
-};
+const genres = [
+	{
+		id: 28,
+		name: "Acción",
+	},
+	{
+		id: 12,
+		name: "Aventura",
+	},
+	{
+		id: 16,
+		name: "Animación",
+	},
+	{
+		id: 35,
+		name: "Comedia",
+	},
+	{
+		id: 80,
+		name: "Crimen",
+	},
+	{
+		id: 99,
+		name: "Documental",
+	},
+	{
+		id: 18,
+		name: "Drama",
+	},
+	{
+		id: 10751,
+		name: "Familia",
+	},
+	{
+		id: 14,
+		name: "Fantasía",
+	},
+	{
+		id: 36,
+		name: "Historia",
+	},
+	{
+		id: 27,
+		name: "Terror",
+	},
+	{
+		id: 10402,
+		name: "Música",
+	},
+	{
+		id: 9648,
+		name: "Misterio",
+	},
+	{
+		id: 10749,
+		name: "Romance",
+	},
+	{
+		id: 878,
+		name: "Ciencia ficción",
+	},
+	{
+		id: 10770,
+		name: "Película de TV",
+	},
+	{
+		id: 53,
+		name: "Suspense",
+	},
+	{
+		id: 10752,
+		name: "Bélica",
+	},
+	{
+		id: 37,
+		name: "Western",
+	},
+];
 
 const movies = document.getElementById("movies");
 const search = document.getElementById("search");
@@ -103,7 +101,11 @@ const getMovies = (url) => {
 		.then((res) => res.json())
 		.then((data) => {
 			console.log(data.results);
-			showMovies(data.results);
+			if (data.results.length !== 0) {
+				showMovies(data.results);
+			} else {
+				movies.innerHTML = `<h2 class="text-center">No se encontraron peliculas </h2>`;
+			}
 		});
 };
 
@@ -114,7 +116,11 @@ const showMovies = (data) => {
 		const card = `<div class="col-12 col-md-6 col-lg-4 col-xxl-2 mb-5">
 		<div class="card">
 			<img
-				src="${IMG_BASE_URL + poster_path}"
+				src="${
+					poster_path
+						? IMG_BASE_URL + poster_path
+						: "https://picsum.photos/id/237/350/500"
+				}"
 				class="card-img-top"
 				alt="${title}"
 			/>
@@ -151,6 +157,8 @@ form.addEventListener("submit", (e) => {
 	e.preventDefault();
 
 	searchTerm = search.value;
+	selectedGenre = [];
+	setGenre();
 
 	if (searchTerm) {
 		getMovies(SEARCH_URL + "&language=es&query=" + searchTerm);
@@ -160,6 +168,73 @@ form.addEventListener("submit", (e) => {
 		getMovies(API_URL);
 	}
 });
+
+// Filter by Genres
+let selectedGenre = [];
+
+const setGenre = () => {
+	tags.innerHTML = "";
+	genres.forEach((genre) => {
+		const genreLi = document.createElement("LI");
+		genreLi.classList.add("dropdown-item");
+		genreLi.id = genre.id;
+		genreLi.textContent = genre.name;
+		genreLi.addEventListener("click", () => {
+			if (selectedGenre.length === 0) {
+				selectedGenre.push(genre.id);
+			} else {
+				if (selectedGenre.includes(genre.id)) {
+					selectedGenre.forEach((id, idx) => {
+						if (id === genre.id) {
+							selectedGenre.splice(idx, 1);
+						}
+					});
+				} else {
+					selectedGenre.push(genre.id);
+				}
+			}
+			console.log(selectedGenre);
+			movies.innerHTML = "";
+			getMovies(API_URL + "&with_genres=" + encodeURI(selectedGenre.join(",")));
+			highlightSelection();
+		});
+		tags.append(genreLi);
+	});
+};
+setGenre();
+
+// Show genre selected
+const highlightSelection = () => {
+	const tags = document.querySelectorAll(".dropdown-item");
+	tags.forEach((tag) => {
+		tag.classList.remove("highlight");
+	});
+	clearBtn();
+	if (selectedGenre.length != 0) {
+		selectedGenre.forEach((id) => {
+			const highlightedTag = document.getElementById(id);
+			highlightedTag.classList.add("highlight");
+		});
+	}
+};
+
+// Clear filter buttons
+const clearBtn = () => {
+	const clearBtn = document.getElementById("clear");
+	if (!clearBtn) {
+		const clear = document.createElement("LI");
+		clear.classList.add("dropdown-item", "bg-info");
+		clear.id = "clear";
+		clear.textContent = "Limpiar filtros";
+		clear.addEventListener("click", () => {
+			selectedGenre = [];
+			setGenre();
+			movies.innerHTML = "";
+			getMovies(API_URL);
+		});
+		tags.append(clear);
+	}
+};
 
 // Painting default results
 getMovies(API_URL);
