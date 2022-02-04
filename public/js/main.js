005c29f7bd29df5386e338644268640a4f22bd05
Luka Liuzzi <lukaliuzzidev@gmail.com>
2022-02-04 19:19:58 -0300

    popular movies slider layout

--- public/js/main.js
+++ public/js/main.js
@@ -293,24 +293,31 @@
 // getMovies(API_URL);
 
 // SWIPER
-const swiper = new Swiper(".swiper", {
-	// Optional parameters
-	direction: "horizontal",
+let swiper = new Swiper(".swiper-container", {
+	slidesPerView: 5,
+	spaceBetween: 10,
+	slidesPerGroup: 2,
 	loop: true,
-
-	// If we need pagination
-	pagination: {
-		el: ".swiper-pagination",
-	},
-
-	// Navigation arrows
 	navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
 	},
-
-	// And if we need scrollbar
-	scrollbar: {
-		el: ".swiper-scrollbar",
+	breakpoints: {
+		100: {
+			slidesPerView: 2,
+			spaceBetween: 10,
+		},
+		640: {
+			slidesPerView: 3,
+			spaceBetween: 10,
+		},
+		768: {
+			slidesPerView: 4,
+			spaceBetween: 10,
+		},
+		1024: {
+			slidesPerView: 5,
+			spaceBetween: 10,
+		},
 	},
 });
